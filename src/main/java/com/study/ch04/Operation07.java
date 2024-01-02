02efa8a5907166b002f3e8c2edd7fabb47715940
seowoo <dkr0210@naver.com>
2024-01-02 17:38:06 +0900

    자격증명 원상복구

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -5,6 +5,7 @@ public class Operation07 {
 
         System.out.println("깃, 깃허브 수업 실습");
         System.out.println("변경사항 추가");
+        System.out.println("git config user.name & user.email");
 
     }
 }

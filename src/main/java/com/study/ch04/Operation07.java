41023d50689e2b050abbebdd6f7a35a97e48af8a
seowoo <dkr0210@naver.com>
2024-01-02 17:13:23 +0900

    본인 이름 추가

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -3,5 +3,7 @@ package com.study.ch04;
 public class Operation07 {
     public static void main(String[] args) {
         System.out.println("깃, 깃허브 수업 실습");
+        System.out.println("변경사항 추가");
+
     }
 }

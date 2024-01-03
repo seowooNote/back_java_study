5e86570eb9a880092df560c49a616921f5c86d81
SeowooNote <dkr0210@naver.com>
2024-01-03 22:28:48 +0900

    20240103-과제

--- src/main/java/com/study/ch06/For03.java
+++ src/main/java/com/study/ch06/For03.java
@@ -10,8 +10,77 @@ public class For03 {
         System.out.print("별 몇개?: ");
         int starCount = scanner.nextInt();
 
+        /*
+        * *
+        * **
+        * ***
+        */
+
         for(int i = 0; i < starCount; i++){
             for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+        * ***
+        * **
+        * *
+        */
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+        *    *
+        *   **
+        *  ***
+        */
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = starCount - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+        * ***
+        *  **
+        *   *
+        */
+
+        System.out.println("--");
+
+        for(int i = 1; i <= starCount; i++) {
+            for(int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = starCount; k >= i; k--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        /*
+        *   *
+        *  ***
+        * *****
+        */
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - i + 1 ; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k < i * 2 - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
